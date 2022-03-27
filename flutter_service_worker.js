444ea4e27b1cab34f32c1795880cff357958d79a
Austin Stoker <austin@austinstoker.com>
2022-03-27 02:24:08 -0600

    Pushing Demo

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "bfa85c3623090e0eb76959ce75072aa5",
+"index.html": "d1788bfc8247e6e49e8f4aaa773c1ac7",
  
 "main.dart.js": "379c31a0f1d23591be01ab643f6505cd",
 "manifest.json": "0582d3e81039abc45295a73912c26140",
